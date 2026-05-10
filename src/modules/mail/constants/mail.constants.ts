56180009200203340191e00e1533d96eeaa11e55
Prasanna <prap.sin.25@gmail.com>
2026-05-10 10:09:19 +0530

    add mail constants for queue name and subjects

---
+++ src/modules/mail/constants/mail.constants.ts
@@ -0,0 +1,7 @@
+export const MAIL_QUEUE_NAME='mail-queue';
+
+export const MAIL_SUBJECTS:Record<string,string>={
+    welcome:"Welcome to our platform!",
+    otp:"Your One-Time Password for the verification!"
+}
+
